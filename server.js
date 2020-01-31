7772464da95fff2aac324ee488473eb48041a200
jsk900 <jsk900@yahoo.co.uk>
2020-01-31 18:21:46 +0100

    Addition of CORS handling

--- server.js
+++ server.js
@@ -13,6 +13,16 @@ app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send('Server running'));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  if (req.method === 'OPTIONS') {
+    res.header('Access=Control-Allow-Methods', 'POST, PUT, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
+
 //Define routes
 
 app.use('/api', require('./routes/api/users'));
