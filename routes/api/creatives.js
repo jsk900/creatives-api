a76f1a334f43f64aa1c88de785296e69bbdd9913
jsk900 <jsk900@yahoo.co.uk>
2020-02-18 12:35:10 +0100

    changes

--- routes/api/creatives.js
+++ routes/api/creatives.js
@@ -153,7 +153,7 @@ router.post(
         { expiresIn: 360000 },
         (err, token) => {
           if (err) throw err;
-          res.json({ token, user });
+          res.json({ token, creative });
         }
       );
     } catch (err) {
