096348098540d3a598c755f41506f7de5ad82423
jsk900 <jsk900@yahoo.co.uk>
2020-02-26 13:50:33 +0100

    test

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json({ extended: false }));
 // app.use(helmet.noCache()); //Disables client side caching
 
 //Enable cors
-//app.use(cors());
+app.use(cors());
 
 app.get('/', (req, res) => res.send('Server running'));
 
